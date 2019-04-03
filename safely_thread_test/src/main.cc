853bb1b89d9f68eecdef0e172485ebfefa806d6d
linzhenhua <linzhenhualzh@163.com>
2019-04-03 19:22:12 +0800

    modify

--- safely_thread_test/src/main.cc
+++ safely_thread_test/src/main.cc
@@ -9,7 +9,7 @@ void do_work(unsigned id)
     //std::cout << "id = " << id << std::endl;
 }
 
-int main(int argc, char** argv)
+void thread_test()
 {
     std::vector<std::thread> threads;
 
@@ -17,18 +17,68 @@ int main(int argc, char** argv)
         threads.push_back(std::thread(do_work, i));
     }
 
-	//由于thread是禁止复制构造函数，Deleted constructor form (thread objects cannot be copied).
-	//所以[](std::thread th)不行，需要引用[](std::thread &th)
-    std::for_each(threads.begin(), threads.end(), [](std::thread &th) { th.join(); /*std::cout << "hello world" << std::endl;*/ });
+    //由于thread是禁止复制构造函数，Deleted constructor form (thread objects cannot be copied).
+    //所以[](std::thread th)不行，需要引用[](std::thread &th)
+    std::for_each(threads.begin(), threads.end(), [](std::thread& th) { th.join(); /*std::cout << "hello world" << std::endl;*/ });
 
     //std::for_each(threads.begin(), threads.end(), std::mem_fn(&std::thread::join));   //可以
 
-	//实际要这样写比较好
-	//for (auto& thread : threads) {   
- //       thread.join();
- //   } 
+    //实际要这样写比较好
+    //for (auto& thread : threads) {
+    //       thread.join();
+    //   }
 
-	std::cout << std::thread::hardware_concurrency() << std::endl;
+    std::cout << std::thread::hardware_concurrency() << std::endl;
+}
+
+//线程安全测试 begin
+class Test1 {
+public:
+    Test1(int i)
+    {
+        val_ = new int(i);
+    }
+
+    ~Test1()
+    {
+        delete val_;
+        val_ = nullptr;
+    }
+
+    void print()
+    {
+        std::cout << "val_ = " << *val_ << std::endl;
+    }
+
+private:
+    int *val_;
+};
+
+class Test2 {
+public:
+    Test2(const Test1* t)
+        : t1_(t)
+    {
+    }
+
+	~Test2(){}
+
+	void print()
+    {
+        std::cout << "t1_ = " << t1_->print() << std::endl;
+    }
+
+private:
+    Test1* t1_;
+};
+
+//线程安全测试 end
+
+int main(int argc, char** argv)
+{
+    Test1 t(1);
+
+	t.print();
 
     return 0;
 }
